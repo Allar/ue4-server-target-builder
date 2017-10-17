896eee25915250547e3a61aa4fe808749bd7eb82
Allar <allar@michaelallar.com>
2017-10-16 19:08:23 -0700

    Won't generate if C++ server target detected

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var cli = require('cli');
 const path = require('path');
 const fs = require('fs-extra');
 const replace = require('replace-in-file');
+const klawSync = require('klaw-sync')
 
 options = cli.parse({
   projectname: [ 'p', 'Project Name', 'string' ],
   sourcefolder: [ 'd', 'Path to folder', 'path'],
-  intermediate: [ 't', 'Generate Temp Target (Use Intermediate folder)', 'bool', false]
+  intermediate: [ 't', 'Generate Temp Target (Use Intermediate folder)', 'bool', false],
+  forceGenerate: [ 'f', 'Force generate, even if c++ server target exists', 'bool', false]
 });
 
 if (options.projectname == null) {
@@ -21,10 +23,29 @@ if (options.sourcefolder == null) {
   return;
 }
 
+// Check to see if server target already exists in non-temp folder
+var bSkipGeneration = false;
+if (!options.forceGenerate) {
+  try {
+    const filterFn = item => item.path.indexOf(options.projectname + 'Server.Target.cs') >= 0
+    const paths = klawSync(path.join(options.sourcefolder, 'Source'), {nodir: true, filterFn});
+    if (paths.length > 0) {
+      bSkipGeneration = true;
+    }
+  } catch (error){
+    // Source folder doesn't exist
+  }
+}
+
+if (bSkipGeneration) {
+  console.log("Skipping target generation, C++ server target detected.");
+  process.exit(0);
+  return;
+}
+
 var SourceFolder = 'Source';
 
 if (options.intermediate) {
-  console.log('ass');
   SourceFolder = path.join("Intermediate", SourceFolder);
 }
 
